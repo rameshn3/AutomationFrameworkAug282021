48ee82d76254fabf3ecb417af1450cba6b40c445
Ramesh <rameshn3@gmail.com>
2021-08-28 08:54:06 +0530

    fixed the compilation issues

--- src/test/java/com/qa/linkedin/page/BrowsePage.java
+++ src/test/java/com/qa/linkedin/page/BrowsePage.java
@@ -1,5 +1,13 @@
-public void BrowsePage extends BasePageWeb{
-private Logger log=Logger.getLogger("BrowsePage");
+package com.qa.linkedin.page;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
+import org.testng.log4testng.Logger;
+public class BrowsePage extends BasePageWeb{
+private Logger log=Logger.getLogger(BrowsePage.class);
 
 public BrowsePage(){
 
